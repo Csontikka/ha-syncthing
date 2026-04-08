6ddcf40d5aac30d1d73bac0bc88b19b322264d3a
Csontikka <csontikka@users.noreply.github.com>
2026-04-08 12:40:40 +0200

    fix: use mdi:folder-lock icon for folder pause button

--- custom_components/syncthing/button.py
+++ custom_components/syncthing/button.py
@@ -135,7 +135,7 @@ class SyncthingFolderPauseButton(CoordinatorEntity[SyncthingCoordinator], Button
 
     _attr_has_entity_name = True
     _attr_translation_key = "folder_pause"
-    _attr_icon = "mdi:folder-off-outline"
+    _attr_icon = "mdi:folder-lock"
 
     def __init__(
         self,
